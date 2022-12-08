3a4c2bc59f2156475370901d348364a19a99fa1b
Myat Kyaw Tun <myatktun2k@gmail.com>
2022-12-08 18:54:39 +0630

    terraform(eks): create EKS cluster using modules
    eks.tf       : create EKS cluster of version 1.23, using managed node
                   groups of maximum 1 instance of on-demand t2.small
    variables.tf : add default values as the files should be ready to use,
                   values can be overwritten from *.tfvars file

---
+++ EKS/terraform/using-modules/eks.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.4"
+
+  cluster_name    = "terraform-eks"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 1
+
+      labels = {
+        role = "general"
+      }
+      instance_types = ["t2.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+}
--- EKS/terraform/using-modules/variables.tf
+++ EKS/terraform/using-modules/variables.tf
@@ -1,39 +1,49 @@
 variable "aws_region" {
-  type = string
+  type    = string
+  default = "ap-southeast-1"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
+  default = "10.0.0.0/18"
 }
 
 variable "vpc_azs" {
-  type = list(string)
+  type    = list(string)
+  default = ["ap-southeast-1a", "ap-southeast-1b"]
 }
 
 variable "vpc_public_subnets" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.32.0/20", "10.0.48.0/20"]
 }
 
 variable "vpc_private_subnets" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.0.0/20", "10.0.16.0/20"]
 }
 
 variable "vpc_enable_nat_gateway" {
-  type = bool
+  type    = bool
+  default = true
 }
 
 variable "vpc_single_nat_gateway" {
-  type = bool
+  type    = bool
+  default = true
 }
 
 variable "vpc_one_nat_gateway_per_az" {
-  type = bool
+  type    = bool
+  default = false
 }
 
 variable "vpc_enable_dns_hostnames" {
-  type = bool
+  type    = bool
+  default = true
 }
 
 variable "vpc_enable_dns_support" {
-  type = bool
+  type    = bool
+  default = true
 }
